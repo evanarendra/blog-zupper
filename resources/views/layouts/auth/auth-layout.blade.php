fed219187854476ff69be56ee230a5d0f7cd7b57
evanarendra <evanarendra7@gmail.com>
2024-07-09 02:54:11 +0700

    commit login form

--- resources/views/layouts/auth/auth-layout.blade.php
+++ resources/views/layouts/auth/auth-layout.blade.php
@@ -1,15 +1,31 @@
+@php
+   $app = [
+         'name' => config('app.name'),
+         'lang' => str_replace('_', '-', app()->getLocale()),
+   ];
+@endphp
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ $app['lang']}}">
 
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <title>Auth Layout</title>
+   <title>{{ $app['name'] }} | @yield('title')</title>
+   @vite('resources/sass/app.scss')
 </head>
 
-<body>
-   @yield('content')
+<body class="bg-primary">
+   <div id="layoutAuthentication">
+      <div id="layoutAuthentication_content">
+         <main>
+            @yield('content')
+         </main>
+      </div>
+      @include('layouts.auth._footer')
+   </div>
+   @vite('resources/js/app.js')
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,5 @@ use App\Http\Controllers\Dashboard\HomeController;
 
 Route::get('/', [SiteHomeController::class, 'index'])->name('site.home');
 Route::get('/login', [LoginController::class, 'index'])->name('auth.login');
+Route::post('/login', [LoginController::class, 'action'])->name('auth.login.action');
 Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard.home');
